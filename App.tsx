319dfe4f08d2ba799d631cb274cd0143b03b3976
frb21 <francisreybetonio46@gmail.com>
2025-04-02 10:39:57 +0800

    Minor changes in App.tsx file

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import WelcomePage  from './components/index';
-//import { StatusBar } from 'expo-status-bar';
-import Signup from './components/signup';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import WelcomePage from './components/index';
+import Signup from './components/signup';
 
 import './global.css';
 
@@ -11,19 +11,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-     <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen 
-        name="Welcome"
-        component={WelcomePage}
-        options={{ headerShown: false }}
-      />
-    
-      <Stack.Screen 
-        name="Signup"
-        component={Signup}
-
-      />
-     </Stack.Navigator>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomePage} options={{ headerShown: false }} />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
